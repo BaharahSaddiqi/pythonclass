1311682f9a5143ccbc9cf5dbd6df93086b74e884
Baharah <Baharahsaddiqi21@gmail.com>
2022-09-14 21:21:15 +0200

    add neu Ex

--- multiplayNumbers.py
+++ multiplayNumbers.py
@@ -1,7 +1,7 @@
-#Baharah 6
-number = int(input("Enter number"))
+#Baharah 6 this is just for test
+number1 = int(input("Enter number"))
 mul = 1
 
-for num in range(1, number +1, 1):
+for num in range(1, number1 +1, 1):
     mul = mul * num
-print("mul of first ", number, " is: ", mul)
+print("mul of first ", number1, " is: ", mul)
